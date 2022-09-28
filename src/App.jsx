2131505f93fcd2ab04724dd4caae3e0d8d257e9f
Fayed Mauyag <faemauyag13@gmail.com>
2022-09-28 16:11:41 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import './assets/App.css'
+import React, { useState } from 'react'
+import InputGroup from './components/inputGroup'
+import Todolist from './components/todoList'
 
 function App() {
-  return (
-    <div className=''>
 
+    const [todo, setTodo] = useState("")
+
+    return (
+    <div className="background">
+        <div className="card-container">
+            <div className="card-body">
+                <h1 className="card-title">Todo App</h1>
+                <p className="card-description">
+                A simple todo app. You can start by adding a todo to the list.
+                </p>
+               <InputGroup todo={todo} setTodo={setTodo} />
+               <Todolist />
+            </div>
+        </div>
     </div>
-  )
+    )
 }
 
 export default App
